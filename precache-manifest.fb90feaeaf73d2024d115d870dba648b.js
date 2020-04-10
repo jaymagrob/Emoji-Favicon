1d119e112320bf53d2af359523876f8b60893d66
jaymagrob <johnmagnusrobertson@gmail.com>
2020-04-10 12:58:31 +0100

    Updates

---
+++ precache-manifest.fb90feaeaf73d2024d115d870dba648b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c029ba13955afc7b1eb3b7fbc2a20d",
+    "url": "/Emoji-Favicon/index.html"
+  },
+  {
+    "revision": "4657129b1e549bf3e606",
+    "url": "/Emoji-Favicon/static/css/main.da022f6b.chunk.css"
+  },
+  {
+    "revision": "33a3b376e62e76c4dfbe",
+    "url": "/Emoji-Favicon/static/js/2.47ccf955.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Emoji-Favicon/static/js/2.47ccf955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4657129b1e549bf3e606",
+    "url": "/Emoji-Favicon/static/js/main.830d1907.chunk.js"
+  },
+  {
+    "revision": "7097fc37b34211ebef21",
+    "url": "/Emoji-Favicon/static/js/runtime-main.9a972e43.js"
+  }
+]);
